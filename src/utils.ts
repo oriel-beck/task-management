b21a7a60ed9ea949e23915a5f93882adb8654465
oriel-beck <orielbeck@gmail.com>
2024-09-09 21:20:15 +0300

    fix disabled sort state

--- src/utils.ts
+++ src/utils.ts
@@ -3,12 +3,12 @@ import { ExtendedTask } from "./store/task.store";
 
 export function sortExtendedTasks(data: ExtendedTask[], sort: Sort) {
     const isAsc = sort.direction === 'asc';
-
+    if (!sort.direction) return data;
     return data.sort((a, b) => {
         let comparison = 0;
         // General comparison for other fields (assuming string or number)
         const valueA = a[sort.active as keyof typeof a];
-        const valueB = b[sort.active as keyof typeof a];
+        const valueB = b[sort.active as keyof typeof b];
 
         if (typeof valueA === 'string' && typeof valueB === 'string') {
             comparison = valueA.localeCompare(valueB);
